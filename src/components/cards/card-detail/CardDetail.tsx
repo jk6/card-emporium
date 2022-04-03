d8d492e87f889fd158752e027e2b75073f16d4a6
Jason <jasonkelly6@gmail.com>
2022-04-03 17:47:28 -0400

    create CardDetail component

---
+++ src/components/cards/card-detail/CardDetail.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import { IMagicCard } from "../../../data/models/IMagicCard";
+
+
+const CardDetail = () => {
+    // let params = useParams();
+    return (
+        <div>detail</div>
+    );
+
+}
+
+export default CardDetail;
