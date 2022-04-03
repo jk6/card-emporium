189a431c21dce1629d982d03f7f8a24516a525e8
Jason <jasonkelly6@gmail.com>
2022-04-03 17:51:19 -0400

    modify pageSize

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,7 +3,8 @@ import { IMagicCard } from './models/IMagicCard';
 
 export const getAllCards = (): Promise<IMagicCard[]> => {
     // #### would normally place this URL in .env file and access via process.ENV variable ####
-    const url = 'https://api.magicthegathering.io/v1/cards';
+    // TODO: come up with paging solution and retrieve all records dynamically
+    const url = 'https://api.magicthegathering.io/v1/cards?pageSize=12';
     return axios
         .get(url)
         .then((res) => res.data.cards)
